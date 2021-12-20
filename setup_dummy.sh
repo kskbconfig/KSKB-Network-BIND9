5debcb106dfd8b2427f90f97574965060d2523f3
KusakabeSi <si@kskb.eu.org>
2021-12-21 01:11:44 +0800

    setup_dummy

---
+++ setup_dummy.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+ip link add dn42-dummy type dummy || true
+ip link set dn42-dummy up || true
+ip addr add 172.22.77.47 dev dn42-dummy || true
+ip addr add fd28:cb8f:4c92:bbbb::53 dev dn42-dummy || true
